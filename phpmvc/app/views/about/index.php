7aedaa87b5ac54cc57c25ec59e5af9bcdf4cc779
michaelnababan <71373620+michaelnababan@users.noreply.github.com>
2020-10-04 20:36:56 +0700

    Menambahkan File Tugas ke 4

---
+++ phpmvc/app/views/about/index.php
@@ -0,0 +1,8 @@
+<div class="container">
+
+    <h1 class="mt-4">About Me</h1>
+    <img src="<?= BASEURL; ?>/img/logo.png" alt="Michael Nababan" width="300"  class="rounded-circle shadow">
+    <br><br>
+    <p>Halo, nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?> tahun, saya adalah seorang <?= $data['pekerjaan']; ?>.</p>
+
+</div>
